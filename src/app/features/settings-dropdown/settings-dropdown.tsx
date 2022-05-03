leather-io/extension

feat: support Ledger hardware wallets

src/app/features/settings-dropdown/settings-dropdown.tsx
@@ -1,62 +1,27 @@
-import { memo, useCallback, useRef } from 'react';
+import { useCallback, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Box, SlideFade, BoxProps, color, Flex } from '@stacks/ui';
+import { Box, SlideFade, color, Flex } from '@stacks/ui';
 
-import { Text, Caption } from '@app/components/typography';
+import { Caption } from '@app/components/typography';
 import { useOnClickOutside } from '@app/common/hooks/use-onclickoutside';
 import { useWallet } from '@app/common/hooks/use-wallet';
 import { useDrawers } from '@app/common/hooks/use-drawers';
 import { RouteUrls } from '@shared/route-urls';
 import { Divider } from '@app/components/divider';
-import { forwardRefWithAs } from '@stacks/ui-core';
 import { SettingsSelectors } from '@tests/integration/settings.selectors';
 import { AccountStep } from '@app/store/ui/ui.models';
 import { useAnalytics } from '@app/common/hooks/analytics/use-analytics';
+import { SettingsMenuItem as MenuItem } from './components/settings-menu-item';
+import { MenuWrapper } from './components/settings-menu-wrapper';
+import { useAccounts } from '@app/store/accounts/account.hooks';
+import { useWalletType } from '@app/common/use-wallet-type';
+import { LedgerDeviceItemRow } from './components/ledger-item-row';
+import { useCurrentKeyDetails } from '@app/store/keys/key.selectors';
+import { extractDeviceNameFromKnownTargetIds } from '../ledger/ledger-utils';
 
-const MenuWrapper = forwardRefWithAs((props, ref) => (
-  <Box
-    ref={ref}
-    position="absolute"
-    top="60px"
-    right="extra-loose"
-    borderRadius="10px"
-    width="296px"
-    boxShadow="0px 8px 16px rgba(27, 39, 51, 0.08);"
-    zIndex={2000}
-    border="1px solid"
-    bg={color('bg')}
-    borderColor={color('border')}
-    py="tight"
-    transformOrigin="top right"
-    {...props}
-  />
-));
-
-const MenuItem = memo((props: BoxProps) => {
-  const { onClick, children, ...rest } = props;
-  return (
-    <Text
-      width="100%"
-      px="base"
-      py="base-tight"
-      cursor="pointer"
-      color={color('text-title')}
-      _hover={{ backgroundColor: color('bg-4') }}
-      onClick={e => {
-        onClick?.(e);
-      }}
-      fontSize={1}
-      {...rest}
-    >
-      {children}
-    </Text>
-  );
-});
-
-export const SettingsDropdown = () => {
+export function SettingsDropdown() {
   const ref = useRef<HTMLDivElement | null>(null);
-  const { lockWallet, wallet, currentNetworkKey, hasGeneratedWallet, encryptedSecretKey } =
-    useWallet();
+  const { lockWallet, currentNetworkKey, hasGeneratedWallet } = useWallet();
   const {
     setShowNetworks,
     setShowAccounts,
@@ -67,10 +32,11 @@ export const SettingsDropdown = () => {
   } = useDrawers();
   const navigate = useNavigate();
   const analytics = useAnalytics();
+  const { walletType } = useWalletType();
+  const key = useCurrentKeyDetails();
 
-  const handleClose = useCallback(() => {
-    setShowSettings(false);
-  }, [setShowSettings]);
+  const handleClose = useCallback(() => setShowSettings(false), [setShowSettings]);
+  const accounts = useAccounts();
 
   const wrappedCloseCallback = useCallback(
     (callback: () => void) => () => {
@@ -88,9 +54,12 @@ export const SettingsDropdown = () => {
     <SlideFade initialOffset="-20px" timeout={150} in={isShowing}>
       {styles => (
         <MenuWrapper ref={ref} style={styles} pointerEvents={!isShowing ? 'none' : 'all'}>
+          {key && key.type === 'ledger' && (
+            <LedgerDeviceItemRow deviceType={extractDeviceNameFromKnownTargetIds(key.targetId)} />
+          )}
           {hasGeneratedWallet && (
             <>
-              {wallet && wallet?.accounts?.length > 1 && (
+              {accounts && accounts.length > 1 && (
                 <MenuItem
                   data-testid={SettingsSelectors.SwitchAccount}
                   onClick={wrappedCloseCallback(() => {
@@ -101,23 +70,25 @@ export const SettingsDropdown = () => {
                   Switch account
                 </MenuItem>
               )}
-              <MenuItem
-                data-testid={SettingsSelectors.BtnCreateAccount}
-                onClick={wrappedCloseCallback(() => {
-                  setAccountStep(AccountStep.Create);
-                  setShowAccounts(true);
-                })}
-              >
-                Create an Account
-              </MenuItem>
-              <MenuItem
-                data-testid={SettingsSelectors.ViewSecretKeyListItem}
-                onClick={wrappedCloseCallback(() => {
-                  navigate(RouteUrls.ViewSecretKey);
-                })}
-              >
-                View Secret Key
-              </MenuItem>
+              {walletType === 'software' && (
+                <>
+                  <MenuItem
+                    data-testid={SettingsSelectors.BtnCreateAccount}
+                    onClick={wrappedCloseCallback(() => {
+                      setAccountStep(AccountStep.Create);
+                      setShowAccounts(true);
+                    })}
+                  >
+                    Create an Account
+                  </MenuItem>
+                  <MenuItem
+                    data-testid={SettingsSelectors.ViewSecretKeyListItem}
+                    onClick={wrappedCloseCallback(() => navigate(RouteUrls.ViewSecretKey))}
+                  >
+                    View Secret Key
+                  </MenuItem>
+                </>
+              )}
             </>
           )}
           {hasGeneratedWallet ? <Divider /> : null}
@@ -133,10 +104,10 @@ export const SettingsDropdown = () => {
               <Caption data-testid={SettingsSelectors.CurrentNetwork}>{currentNetworkKey}</Caption>
             </Flex>
           </MenuItem>
-          {encryptedSecretKey && (
+          {hasGeneratedWallet && (
             <>
               <Divider />
-              {hasGeneratedWallet && (
+              {walletType === 'software' && (
                 <MenuItem
                   onClick={wrappedCloseCallback(() => {
                     void analytics.track('lock_session');
@@ -164,4 +135,4 @@ export const SettingsDropdown = () => {
       )}
     </SlideFade>
   );
-};
+}

timstackblock/stacks-wallet-web

src/app/features/settings-dropdown/settings-dropdown.tsx
@@ -1,79 +1,39 @@
-import { memo, useCallback, useRef } from 'react';
+import { useCallback, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Box, SlideFade, BoxProps, color, Flex } from '@stacks/ui';
+import { Box, SlideFade, color, Flex } from '@stacks/ui';
 
-import { Text, Caption } from '@app/components/typography';
+import { Caption } from '@app/components/typography';
 import { useOnClickOutside } from '@app/common/hooks/use-onclickoutside';
 import { useWallet } from '@app/common/hooks/use-wallet';
 import { useDrawers } from '@app/common/hooks/use-drawers';
 import { RouteUrls } from '@shared/route-urls';
 import { Divider } from '@app/components/divider';
-import { forwardRefWithAs } from '@stacks/ui-core';
 import { SettingsSelectors } from '@tests/integration/settings.selectors';
 import { useAnalytics } from '@app/common/hooks/analytics/use-analytics';
 import { useCreateAccount } from '@app/common/hooks/account/use-create-account';
 import { useHasCreatedAccount } from '@app/store/accounts/account.hooks';
 import { Overlay } from '@app/components/overlay';
+import { SettingsMenuItem as MenuItem } from './components/settings-menu-item';
+import { MenuWrapper } from './components/settings-menu-wrapper';
+import { useAccounts } from '@app/store/accounts/account.hooks';
+import { useWalletType } from '@app/common/use-wallet-type';
+import { LedgerDeviceItemRow } from './components/ledger-item-row';
+import { useCurrentKeyDetails } from '@app/store/keys/key.selectors';
+import { extractDeviceNameFromKnownTargetIds } from '../ledger/ledger-utils';
 
-const MenuWrapper = forwardRefWithAs((props, ref) => (
-  <Box
-    ref={ref}
-    position="absolute"
-    top="60px"
-    right="extra-loose"
-    borderRadius="10px"
-    width="296px"
-    boxShadow="0px 8px 16px rgba(27, 39, 51, 0.08);"
-    zIndex={2000}
-    border="1px solid"
-    bg={color('bg')}
-    borderColor={color('border')}
-    py="tight"
-    transformOrigin="top right"
-    {...props}
-  />
-));
-
-const MenuItem = memo((props: BoxProps) => {
-  const { onClick, children, ...rest } = props;
-  return (
-    <Text
-      width="100%"
-      px="base"
-      py="base-tight"
-      cursor="pointer"
-      color={color('text-title')}
-      _hover={{ backgroundColor: color('bg-4') }}
-      onClick={e => {
-        onClick?.(e);
-      }}
-      fontSize={1}
-      {...rest}
-    >
-      {children}
-    </Text>
-  );
-});
-
-export const SettingsDropdown = () => {
+export function SettingsDropdown() {
   const ref = useRef<HTMLDivElement | null>(null);
-  const { lockWallet, wallet, currentNetworkKey, hasGeneratedWallet, encryptedSecretKey } =
-    useWallet();
+  const { lockWallet, currentNetworkKey, hasGeneratedWallet, encryptedSecretKey } = useWallet();
   const createAccount = useCreateAccount();
   const [hasCreatedAccount, setHasCreatedAccount] = useHasCreatedAccount();
-  const {
-    setShowNetworks,
-    setShowSwitchAccountsState,
-    setShowSettings,
-    showSettings,
-    setShowSignOut,
-  } = useDrawers();
+  const { setShowNetworks, setShowSettings, showSettings, setShowSignOut } = useDrawers();
   const navigate = useNavigate();
   const analytics = useAnalytics();
+  const { walletType } = useWalletType();
+  const key = useCurrentKeyDetails();
 
-  const handleClose = useCallback(() => {
-    setShowSettings(false);
-  }, [setShowSettings]);
+  const handleClose = useCallback(() => setShowSettings(false), [setShowSettings]);
+  const accounts = useAccounts();
 
   const wrappedCloseCallback = useCallback(
     (callback: () => void) => () => {
@@ -93,38 +53,54 @@ export const SettingsDropdown = () => {
       <SlideFade initialOffset="-20px" timeout={150} in={isShowing}>
         {styles => (
           <MenuWrapper ref={ref} style={styles} pointerEvents={!isShowing ? 'none' : 'all'}>
+            {key && key.type === 'ledger' && (
+              <LedgerDeviceItemRow deviceType={extractDeviceNameFromKnownTargetIds(key.targetId)} />
+            )}
             {hasGeneratedWallet && (
               <>
-                {wallet && wallet?.accounts?.length > 1 && (
+                {walletType === 'software' && (
                   <MenuItem
-                    data-testid={SettingsSelectors.SwitchAccount}
+                    data-testid={SettingsSelectors.CreateAccountBtn}
                     onClick={wrappedCloseCallback(() => {
-                      setShowSwitchAccountsState(true);
+                      void createAccount();
+                      setHasCreatedAccount(true);
                     })}
                   >
-                    Switch account
+                    Create an Account
                   </MenuItem>
                 )}
-                <MenuItem
-                  data-testid={SettingsSelectors.CreateAccountBtn}
-                  onClick={wrappedCloseCallback(() => {
-                    void createAccount();
-                    setHasCreatedAccount(true);
-                  })}
-                >
-                  Create an Account
-                </MenuItem>
-                <MenuItem
-                  data-testid={SettingsSelectors.ViewSecretKeyListItem}
-                  onClick={wrappedCloseCallback(() => {
-                    navigate(RouteUrls.ViewSecretKey);
-                  })}
-                >
-                  View Secret Key
-                </MenuItem>
               </>
             )}
             {hasGeneratedWallet ? <Divider /> : null}
+            <MenuItem
+              data-testid={SettingsSelectors.ChangeNetworkAction}
+              onClick={wrappedCloseCallback(() => {
+                void analytics.track('choose_to_change_network');
+                setShowNetworks(true);
+              })}
+            >
+              <Flex width="100%" alignItems="center" justifyContent="space-between">
+                <Box>Change Network</Box>
+                <Caption data-testid={SettingsSelectors.CurrentNetwork}>
+                  {currentNetworkKey}
+                </Caption>
+              </Flex>
+            </MenuItem>
+            {hasGeneratedWallet && (
+              <>
+                <Divider />
+                {walletType === 'software' && (
+                  <MenuItem
+                    data-testid={SettingsSelectors.ViewSecretKeyListItem}
+                    onClick={wrappedCloseCallback(() => {
+                      navigate(RouteUrls.ViewSecretKey);
+                    })}
+                  >
+                    View Secret Key
+                  </MenuItem>
+                )}
+              </>
+            )}
             <MenuItem
               data-testid={SettingsSelectors.ChangeNetworkAction}
               onClick={wrappedCloseCallback(() => {
@@ -171,4 +147,4 @@ export const SettingsDropdown = () => {
       </SlideFade>
     </>
   );
-};
+}